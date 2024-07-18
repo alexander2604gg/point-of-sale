e642a60f3e11eeb08792b51ec719b4333f0cfb45
Alexander Saul <alexandersaul2604@gmail.com>
2024-07-17 23:26:22 -0500

    product controller

--- src/main/java/com/alexandersaul/PointOfSale/controller/ProductController.java
+++ src/main/java/com/alexandersaul/PointOfSale/controller/ProductController.java
@@ -3,12 +3,12 @@ package com.alexandersaul.PointOfSale.controller;
 import com.alexandersaul.PointOfSale.dto.ProductDTO;
 import com.alexandersaul.PointOfSale.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/api/product")
@@ -22,5 +22,33 @@ public class ProductController {
         List<ProductDTO> productDTOList = productService.getAll();
         return ResponseEntity.ok(productDTOList);
     }
+
+    @PostMapping("/resource")
+    public ResponseEntity<ProductDTO> saveProduct (@RequestBody ProductDTO productDTO) {
+
+        if(productDTO == null){
+            return ResponseEntity.badRequest().build();
+        }
+
+        ProductDTO newProductDTO = productService.save(productDTO);
+
+        if(newProductDTO != null){
+            return ResponseEntity.status(HttpStatus.CREATED).body(newProductDTO);
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+
+    }
+
+    @DeleteMapping("resource/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        try {
+            productService.delete(id);
+            return ResponseEntity.noContent().build();
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
 
 }
