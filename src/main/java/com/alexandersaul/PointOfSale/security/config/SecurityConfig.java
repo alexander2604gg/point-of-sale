f7aacf911784a10ae306b6337e20ef436dcaf53c
Alexander Saul <alexandersaul2604@gmail.com>
2024-07-18 00:18:19 -0500

    security

--- src/main/java/com/alexandersaul/PointOfSale/security/config/SecurityConfig.java
+++ src/main/java/com/alexandersaul/PointOfSale/security/config/SecurityConfig.java
@@ -1,2 +1,41 @@
-package com.alexandersaul.PointOfSale.security.config;public class SecurityConfig {
+package com.alexandersaul.PointOfSale.security.config;
+
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain (HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(http -> {
+                    http.requestMatchers(HttpMethod.GET , "/api/product/resource").permitAll();
+                    http.requestMatchers(HttpMethod.POST , "/api/product/resource").permitAll();
+                })
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager (AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+
+
+
 }
